dcf5230ee1db34915fb37ea8db1000d460f29009
Peter Meulbroek <pmeulbroek@riskfocus.com>
2018-09-04 10:19:46 -0400

    added parameter handling for :since and :all in stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,20 @@
 class StatsController < ApplicationController
   before_action :set_stat, only: [:show ]
   # GET /stats
+  before_action :validate_params
+
   def index
-    @stats = Stat.all
+
+    if params[:all]
+      @stats = Stat.all.select(:time, :value)
+    elsif params[:since]
+      @stats = Stat.where('time > :since', {since: params[:since]}).select(:time, :value)
+    else
+      @stats = Stat.order(time: :asc ).select(:time, :value).last
+    end
+
     json_response(@stats)
+
   end
 
   def shutdown
@@ -17,4 +28,14 @@ class StatsController < ApplicationController
     # corrupt data so the app looks permanently unhealthy
     Stat.create(time: Time.now(), value: -2000)
   end
+
+
+  private
+  
+  def validate_params
+    unless  params.nil?
+      params.permit(:since, :all)
+    end
+  end
+
 end
--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -11,6 +11,21 @@ RSpec.describe 'stats API', type: :request do
     it 'returns stats' do
       # Note `json` is a custom helper to parse JSON responses
       expect(json).not_to be_empty
+      expect(json.size).to eq(3)
+    end
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /stats?all' do
+    # make HTTP get request before each example
+    before { get '/stats?all=true' }
+    
+    it 'returns stats' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
     
@@ -18,4 +33,20 @@ RSpec.describe 'stats API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  
+  describe 'GET /stats?since' do
+    # make HTTP get request before each example
+    before { get '/stats?since="2018-01-01"' }
+    
+    it 'returns stats' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
