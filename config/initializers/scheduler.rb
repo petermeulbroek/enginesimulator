4bb5dd874ff45495c0a01b1ec4d5e3fd11d1446d
Peter Meulbroek <pmeulbroek@riskfocus.com>
2018-09-09 21:13:59 -0400

    fixed minor race condition

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,6 +6,15 @@ Rails.logger.debug 'adding initial data'
 scheduler = Rufus::Scheduler::singleton
 
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
+
+  # add the fake monitoring data
+  scheduler.every Settings.statCreateFrequency, :tags => 'stats', :mutex => 'my_mutex' do
+    Rails.logger.info  'monitoring data'
+    lstat = Stat.order(:time).last
+
+    Stat.create!({time: Time.now(), value: lstat.value + Settings.statRange * (rand - 0.5) })
+  end
+
   
   # add the health status
   scheduler.every Settings.healthCreateFrequency, :tags => 'health', :mutex => 'my_mutex' do 
@@ -22,12 +31,5 @@ unless defined?(Rails::Console) || File.split($0).last == 'rake'
     
   end
   
-  # add the fake monitoring data
-  scheduler.every Settings.statCreateFrequency, :tags => 'stats', :mutex => 'my_mutex' do
-    Rails.logger.info  'monitoring data'
-    lstat = Stat.order(:time).last
-
-    Stat.create!({time: Time.now(), value: lstat.value + Settings.statRange * (rand - 0.5) })
-  end
 
 end
